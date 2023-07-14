41023a5723137d4a0092a409d6ba572dbbf4ab85
leopham <hongthaipro@gmail.com>
2023-07-14 12:29:54 +0700

    Setup program

---
+++ app/artifacts/renext_program.ts
@@ -0,0 +1,23 @@
+export type RenextProgram = {
+  "version": "0.1.0",
+  "name": "renext_program",
+  "instructions": [
+    {
+      "name": "initialize",
+      "accounts": [],
+      "args": []
+    }
+  ]
+};
+
+export const IDL: RenextProgram = {
+  "version": "0.1.0",
+  "name": "renext_program",
+  "instructions": [
+    {
+      "name": "initialize",
+      "accounts": [],
+      "args": []
+    }
+  ]
+};

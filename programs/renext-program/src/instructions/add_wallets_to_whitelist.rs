fb47fc46f5feeae500969a04b71870f130243871
leopham <hongthaipro@gmail.com>
2023-11-10 09:19:50 +0700

    remove checking wallet code existed in whitelist

--- programs/renext-program/src/instructions/add_wallets_to_whitelist.rs
+++ programs/renext-program/src/instructions/add_wallets_to_whitelist.rs
@@ -30,13 +30,6 @@ pub fn handler(ctx: Context<AddWalletsToWhitelist>, wallets: Vec<Pubkey>) -> Pro
 
     require!(wallets.len() > 0, MyError::WalletsMustNotBeEmpty);
 
-    for wallet in wallets.iter() {
-        require!(
-            whitelist.is_pubkey_in_list(&wallet) == false,
-            MyError::WalletAlreadyAdded
-        );
-    }
-
     whitelist.add_list_pubkey(wallets)?;
 
     Ok(())
